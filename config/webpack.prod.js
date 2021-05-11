1290ac5cdc7c6037c4b4dcf9de8626243420c0cb
张力仁 <https://gitee.com/zhang-liren/xiaoxindebaobei.git>
2021-05-11 11:05:52 +0800

    5.10

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,6 +5,9 @@ const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // 引入CleanWebpackPlugin插件
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const PurifyCssWebpack = require('purifycss-webpack'); // 引入PurifyCssWebpack插件
+const glob = require('glob');  // 引入glob模块,用于扫描全部html文件中所引用的css
 
 module.exports = merge(common, { // 将webpack.common.js合并到当前文件
     mode:'production',
@@ -18,6 +21,9 @@ module.exports = merge(common, { // 将webpack.common.js合并到当前文件
         }),
         new MiniCssExtractPlugin({
             filename: './css/[name].css'
+        }),
+        new PurifyCssWebpack({
+            paths: glob.sync(path.join(__dirname, 'src/*.html')) // 同步扫描所有html文件中所引用的css
         })
     ]
 })
